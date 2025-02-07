ec4c687bff1e17c712215bc7b1923f49891d7a5e
akgupta2002D <akgupta2002d@gmail.com>
2025-02-07 16:22:08 -0500

    Update for rose day

--- app/components/HeartComponent.js
+++ app/components/HeartComponent.js
@@ -3,9 +3,18 @@ import styles from "../styles/HeartComponent.module.css";
 
 const HeartComponent = ({ onClick }) => {
   return (
+    <>
     <div className={`${styles.image} ${styles.grow}`} onClick={onClick}>
-      <img src="/heart.png" alt="Heart" className={styles.img} />
+      <img src="/rose.png" alt="Heart" className={styles.img} />
     </div>
+    <div className={styles.messageContainer}>
+      <p>Hey Vedu! 😊</p>
+      <p>Thank you for being there in my life. You mean a lot to me.</p>
+      <p className={styles.note}>
+        Every rose may fade, but my love for you blooms forever. Happy Rose Day, my love! 🌹❤️
+      </p>
+    </div>
+    </>
   );
 };
 
